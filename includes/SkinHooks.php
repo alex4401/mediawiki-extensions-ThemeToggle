04a722448d094b2530714ecd0c188e98e9246ba9
alex4401 <rylatgl@gmail.com>
2022-08-27 06:05:19 +0200

    feat: attach settings to the script

--- includes/SkinHooks.php
+++ includes/SkinHooks.php
@@ -12,12 +12,16 @@ class SkinHooks implements BeforePageDisplayHook {
 	 * Injects the inline theme applying script to the document head
 	 */
 	public function onBeforePageDisplay( $out, $skin ): void {
+        global $wgThemingDefault, $wgThemingSiteCssBundled;
+        
 		$nonce = $out->getCSP()->getNonce();
 
 		// modules/inline.js
 		$script = sprintf(
-			'<script%s>%s</script>',
+			'<script%s>SITEDEFAULTTHEME=%s;SITEBUNDLEDTHEMES=%s;%s</script>',
 			$nonce !== false ? sprintf( ' nonce="%s"', $nonce ) : '',
+            json_encode( $wgThemingDefault ),
+            json_encode( $wgThemingSiteCssBundled ),
 			'window.extApplyThemePreference=function(){var e="skin-theme",t="theme-",n=["dark","light"];function o(){return window.localStorage.getItem(e)}var c=o(),a=window.matchMedia("(prefers-color-scheme: dark)");function r(){try{c=o(),null!==c&&(n.forEach(function(e){document.documentElement.classList.remove(t+e)}),document.documentElement.classList.add(t+c))}catch(e){}}function i(){c=a.matches?"dark":"light",window.localStorage.setItem(e,c),r(),window.localStorage.setItem(e,"auto")}"auto"===c?(i(),a.addEventListener("change",i)):(r(),a.removeEventListener("change",i))},window.extApplyThemePreference();'
         );
 
