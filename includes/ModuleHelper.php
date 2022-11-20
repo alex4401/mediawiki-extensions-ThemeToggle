9e18660e1cecb52bafd8df225a9fd6dcd9c72760
alex4401 <rylatgl@gmail.com>
2022-11-20 12:30:10 +0100

    refactor: drop InlineJsConstants

--- includes/ModuleHelper.php
+++ includes/ModuleHelper.php
@@ -28,18 +28,4 @@ class ModuleHelper {
         }
         return 'withAuto';
     }
-
-    /**
-     * Returns the script to be added into the document head.
-     * 
-     * As themes can be managed via MediaWiki:Theme-definitions, do NOT use dark or light to decide if the auto-supporting
-     * payload is best. This should be manually controlled because of cache constraints.
-     */
-    private function getCoreJsToInject(): string {
-        global $wgThemeToggleDisableAutoDetection;
-        if ( $wgThemeToggleDisableAutoDetection ) {
-            return InlineJsConstants::NO_AUTO;
-        }
-        return InlineJsConstants::WITH_AUTO;
-    }
 }
