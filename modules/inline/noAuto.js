c37dc87448b074f744058940f2b8836c76b96d98
alex4401 <rylatgl@gmail.com>
2022-12-02 02:02:35 +0100

    refactor: streamline core script structure

--- modules/inline/noAuto.js
+++ modules/inline/noAuto.js
@@ -3,8 +3,39 @@
 
 ( function () {
     var themeKey = 'skin-theme',
+        htmlNode = document.documentElement,
         linkNode = null,
         currentTheme = null;
+
+
+    function _setThemeImpl() {
+        try {
+            // Apply by changing class
+            if ( currentTheme !== null ) {
+                // Remove all theme classes
+                htmlNode.className = htmlNode.className.replace( / theme-[^\s]+/ig, '' );
+                // Add new theme class
+                htmlNode.classList.add( 'theme-' + currentTheme );
+            }
+
+            if ( VARS.SiteBundledCss.indexOf( currentTheme ) < 0 ) {
+                if ( linkNode === null ) {
+                    linkNode = document.createElement( 'link' );
+                    document.head.appendChild( linkNode );
+                }
+                linkNode.rel = 'stylesheet';
+                linkNode.type = 'text/css';
+                linkNode.href = VARS.ResourceLoaderEndpoint + '&modules=ext.theme.' + currentTheme + '&only=styles';
+            } else if ( linkNode !== null ) {
+                document.head.removeChild( linkNode );
+                linkNode = null;
+            }
+        } catch ( ex ) {
+            setTimeout( function () {
+                throw ex;
+            }, 0 );
+        }
+    }
 
 
     window.MwSkinTheme = {
@@ -14,42 +45,8 @@
 
 
         set: function ( target ) {
-            var htmlNode = document.documentElement;
-
             currentTheme = target;
-
-
-            function applyInternal() {
-                try {
-                    // Apply by changing class
-                    if ( currentTheme !== null ) {
-                        // Remove all theme classes
-                        htmlNode.className = htmlNode.className.replace( / theme-[^\s]+/ig, '' );
-                        // Add new theme class
-                        htmlNode.classList.add( 'theme-' + currentTheme );
-                    }
-
-                    if ( VARS.SiteBundledCss.indexOf( currentTheme ) < 0 ) {
-                        if ( linkNode === null ) {
-                            linkNode = document.createElement( 'link' );
-                            document.head.appendChild( linkNode );
-                        }
-                        linkNode.rel = 'stylesheet';
-                        linkNode.type = 'text/css';
-                        linkNode.href = VARS.ResourceLoaderEndpoint + '&modules=ext.theme.' + currentTheme + '&only=styles';
-                    } else if ( linkNode !== null ) {
-                        document.head.removeChild( linkNode );
-                        linkNode = null;
-                    }
-                } catch ( ex ) {
-                    setTimeout( function () {
-                        throw ex;
-                    }, 0 );
-                }
-            }
-
-
-            applyInternal();
+            _setThemeImpl();
         }
     };
 
--- modules/inline/withAuto.js
+++ modules/inline/withAuto.js
@@ -4,9 +4,44 @@
 ( function () {
     var themeKey = 'skin-theme',
         prefersDark = window.matchMedia( '(prefers-color-scheme: dark)' ),
+        htmlNode = document.documentElement,
         linkNode = null,
-        currentTheme = null,
-        currentThemeActual = null;
+        currentTheme = null;
+
+
+    function _setThemeImpl( actualTarget ) {
+        try {
+            // Apply by changing class
+            if ( actualTarget !== null ) {
+                // Remove all theme classes
+                htmlNode.className = htmlNode.className.replace( / theme-[^\s]+/ig, '' );
+                // Add new theme class
+                htmlNode.classList.add( 'theme-' + actualTarget );
+            }
+
+            if ( VARS.SiteBundledCss.indexOf( actualTarget ) < 0 ) {
+                if ( linkNode === null ) {
+                    linkNode = document.createElement( 'link' );
+                    document.head.appendChild( linkNode );
+                }
+                linkNode.rel = 'stylesheet';
+                linkNode.type = 'text/css';
+                linkNode.href = VARS.ResourceLoaderEndpoint + '&modules=ext.theme.' + actualTarget + '&only=styles';
+            } else if ( linkNode !== null ) {
+                document.head.removeChild( linkNode );
+                linkNode = null;
+            }
+        } catch ( ex ) {
+            setTimeout( function () {
+                throw ex;
+            }, 0 );
+        }
+    }
+
+
+    function _detect() {
+        _setThemeImpl( prefersDark.matches ? 'dark' : 'light' );
+    }
 
 
     window.MwSkinTheme = {
@@ -16,59 +51,18 @@
 
 
         set: function ( target ) {
-            var htmlNode = document.documentElement;
-
             currentTheme = target;
-
-
-            function applyInternal( actualTarget ) {
-                currentThemeActual = actualTarget;
-
-                try {
-                    // Apply by changing class
-                    if ( currentThemeActual !== null ) {
-                        // Remove all theme classes
-                        htmlNode.className = htmlNode.className.replace( / theme-[^\s]+/ig, '' );
-                        // Add new theme class
-                        htmlNode.classList.add( 'theme-' + currentThemeActual );
-                    }
-
-                    if ( VARS.SiteBundledCss.indexOf( currentThemeActual ) < 0 ) {
-                        if ( linkNode === null ) {
-                            linkNode = document.createElement( 'link' );
-                            document.head.appendChild( linkNode );
-                        }
-                        linkNode.rel = 'stylesheet';
-                        linkNode.type = 'text/css';
-                        linkNode.href = VARS.ResourceLoaderEndpoint + '&modules=ext.theme.' + currentThemeActual
-                            + '&only=styles';
-                    } else if ( linkNode !== null ) {
-                        document.head.removeChild( linkNode );
-                        linkNode = null;
-                    }
-                } catch ( ex ) {
-                    setTimeout( function () {
-                        throw ex;
-                    }, 0 );
-                }
-            }
-
-
-            function detectInternal() {
-                applyInternal( prefersDark.matches ? 'dark' : 'light' );
-            }
-
 
             if ( currentTheme === 'auto' ) {
                 // Detect preferred theme by prefers-color-scheme
-                detectInternal();
+                _detect();
                 htmlNode.classList.add( 'theme-auto' );
                 // Attach listener for future changes
-                prefersDark.addEventListener( 'change', detectInternal );
+                prefersDark.addEventListener( 'change', _detect );
             } else {
                 // Apply the theme choice and stop tracking prefers-color-scheme changes
-                applyInternal( currentTheme );
-                prefersDark.removeEventListener( 'change', detectInternal );
+                _setThemeImpl( currentTheme );
+                prefersDark.removeEventListener( 'change', _detect );
             }
         }
     };
