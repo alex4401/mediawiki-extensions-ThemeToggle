b746a84011c1807b1c77e1ca2d539dc1ba7ac65c
alex4401 <rylatgl@gmail.com>
2022-12-06 13:47:59 +0100

    feat: define FeatureInfo structure

---
+++ includes/FeatureInfo.php
@@ -0,0 +1,34 @@
+<?php
+namespace MediaWiki\Extension\ThemeToggle;
+
+use InvalidArgumentException;
+
+class FeatureInfo {
+    private string $id;
+    private bool $default = false;
+
+    public function __construct( array $info ) {
+        foreach ( $info as $option => $params ) {
+            switch ( $option ) {
+                case 'id':
+                case 'default':
+                    $this->{$option} = $params;
+                    break;
+                default:
+                    throw new InvalidArgumentException( "Unrecognized '$option' parameter" );
+            }
+        }
+    }
+
+    public function getId(): string {
+        return $this->id;
+    }
+
+    public function getMessageId(): string {
+        return 'theme-feature-' . $this->id;
+    }
+
+    public function isDefault(): bool {
+        return $this->default;
+    }
+}
