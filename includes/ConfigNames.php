d3818b02d1d2b417155bb4b67b4d669625d09cbe
alex4401 <rylatgl@gmail.com>
2023-07-23 04:00:33 +0200

    feat: ConfigNames class

---
+++ includes/ConfigNames.php
@@ -0,0 +1,42 @@
+<?php
+namespace MediaWiki\Extension\ThemeToggle;
+
+// phpcs:disable Generic.NamingConventions.UpperCaseConstantName.ClassConstantNotUpperCase
+
+/**
+ * A class containing constants representing the names of ThemeToggle' configuration variables. These constants can be
+ * used with ServiceOptions or via ExtensionConfig to protect against typos.
+ *
+ * @since 1.0.0
+ */
+class ConfigNames {
+    /**
+     * Name constant. For use in ExtensionConfig.
+     */
+    public const DefaultTheme = 'ThemeToggleDefault';
+
+    /**
+     * Name constant. For use in ExtensionConfig.
+     */
+    public const DisableAutoDetection = 'ThemeToggleDisableAutoDetection';
+
+    /**
+     * Name constant. For use in ExtensionConfig.
+     */
+    public const SwitcherStyle = 'ThemeToggleSwitcherStyle';
+
+    /**
+     * Name constant. For use in ExtensionConfig.
+     */
+    public const EnableForAnonymousUsers = 'ThemeToggleEnableForAnonymousUsers';
+
+    /**
+     * Name constant. For use in ExtensionConfig.
+     */
+    public const PreferenceGroup = 'ThemeTogglePreferenceGroup';
+
+    /**
+     * Name constant. For use in ExtensionConfig.
+     */
+    public const LoadScriptOverride = 'ThemeToggleLoadScriptOverride';
+}
