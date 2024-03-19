12bf04b0c70e5901ef3aa8e56394c4d4acb41197
alex4401 <rylatgl@gmail.com>
2024-03-19 22:59:58 +0100

    refactor: finishing the wiki.gg dropdown switcher redesign

--- modules/dropdownSwitcher/main.js
+++ modules/dropdownSwitcher/main.js
@@ -3,7 +3,7 @@
 */
 
 var Shared = require( 'ext.themes.jsapi' );
-var $container, $label, $list;
+var $container, $themeHeading, $list;
 
 
 function addTheme( themeId ) {
@@ -20,7 +20,7 @@ function addTheme( themeId ) {
         event.preventDefault();
         Shared.setUserPreference( themeId );
         // eslint-disable-next-line mediawiki/msg-doc
-        $label.innerText = mw.msg( 'theme-' + themeId );
+        $themeHeading.innerText = mw.msg( 'theme-' + themeId );
     } );
 
     $list.appendChild( $item );
@@ -33,11 +33,11 @@ function initialise() {
     var $metaHeading = document.createElement( 'span' );
     $metaHeading.innerText = mw.msg( 'themetoggle-skinprefs' );
 
-    var $themeHeading = document.createElement( 'span' );
+    $themeHeading = document.createElement( 'span' );
     // eslint-disable-next-line mediawiki/msg-doc
     $themeHeading.innerText = mw.msg( 'theme-' + MwSkinTheme.getCurrent() );
 
-    $label = document.createElement( 'label' );
+    var $label = document.createElement( 'label' );
     $label.id = 'pt-themes-label';
     $label.htmlFor = 'pt-themes-checkbox';
     $label.appendChild( $metaHeading );
@@ -51,18 +51,22 @@ function initialise() {
     $toggle.setAttribute( 'aria-labelledby', $label.htmlFor );
     $toggle.title = mw.msg( 'themetoggle-dropdown-switch' );
 
+    var $themeSection = document.createElement( 'li' );
+    $themeSection.innerText = mw.msg( 'themetoggle-dropdown-section-themes' );
+
     $list = document.createElement( 'ul' );
+    $list.appendChild( $themeSection );
+
+    var $popup = document.createElement( 'div' );
+    $popup.className = 'ext-themetoggle-popup';
+    $popup.appendChild( $list );
 
     $container = document.createElement( 'li' );
     $container.id = 'pt-themes';
     $container.className = 'mw-list-item';
     $container.appendChild( $toggle );
     $container.appendChild( $label );
-    $container.appendChild( $list );
-
-    var $themeSection = document.createElement( 'li' );
-    $themeSection.innerText = mw.msg( 'themetoggle-dropdown-section-themes' );
-    $list.appendChild( $themeSection );
+    $container.appendChild( $popup );
 
     if ( Shared.CONFIG.supportsAuto ) {
         addTheme( 'auto' );
