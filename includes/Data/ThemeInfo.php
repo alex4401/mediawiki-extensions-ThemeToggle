911ee6c39fc2884935d559b046a2a6b418c98d4b
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2024-03-22 13:22:59 -0500

    mostly working except js

--- includes/Data/ThemeInfo.php
+++ includes/Data/ThemeInfo.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace MediaWiki\Extension\ThemeToggle\Data;
 
 use InvalidArgumentException;
 
-class ThemeInfo {
+class ThemeInfo
+{
     /** @var string */
     private string $id;
     /** @var string[] Required user groups to advertise. */
@@ -12,32 +14,31 @@ class ThemeInfo {
     private bool $default = false;
     /** @var bool Whether included in site CSS. */
     private bool $bundled = false;
-    /** @var kind Either dark or light */
-	private string $kind = "unknown";
-	/** @var array Allowed kinds & their corresponding classes */
+    /** @var string defaultKind if kind is unknown */
+    private string $defaultKind = 'unknown';
+    /** @var string kind Either dark or light */
+    private string $kind = "";
+    /** @var array Allowed kinds & their corresponding classes */
     private array $recognizedKinds = [
-    	"dark" => "kind-dark",
-		"light" => "kind-light",
-		"unknown" => "kind-unknown",
-	];
+        'dark' => 'view-dark',
+        'light' => 'view-light',
+        'unknown' => 'view-unknown',
+    ];
 
 
-    public function __construct( array $info ) {
-        foreach ( $info as $option => $params ) {
-            switch ( $option ) {
+    public function __construct(array $info)
+    {
+        foreach ($info as $option => $params) {
+            switch ($option) {
                 case 'id':
                 case 'userGroups':
                 case 'default':
                 case 'bundled':
+                case 'kind':
                     $this->{$option} = $params;
                     break;
-				case 'kind':
-					if (array_key_exists($params[0], $this->recognizedKinds)) {
-						$this->kind = $params[0];
-					}
-					break;
                 default:
-                    throw new InvalidArgumentException( "Unrecognized '$option' parameter" );
+                    throw new InvalidArgumentException("Unrecognized '$option' parameter");
             }
         }
     }
@@ -45,42 +46,48 @@ class ThemeInfo {
     /**
      * @return string
      */
-    public function getId(): string {
+    public function getId(): string
+    {
         return $this->id;
     }
 
     /**
      * @return string
      */
-    public function getMessageId(): string {
+    public function getMessageId(): string
+    {
         return 'theme-' . $this->id;
     }
 
     /**
      * @return string
      */
-    public function getCssPageName(): string {
+    public function getCssPageName(): string
+    {
         return 'MediaWiki:Theme-' . $this->id . '.css';
     }
 
     /**
      * @return bool Whether included in site CSS.
      */
-    public function isBundled(): bool {
+    public function isBundled(): bool
+    {
         return $this->bundled;
     }
 
     /**
      * @return bool
      */
-    public function isDefault(): bool {
+    public function isDefault(): bool
+    {
         return $this->default;
     }
 
     /**
      * @return string[]
      */
-    public function getEntitledUserGroups(): array {
+    public function getEntitledUserGroups(): array
+    {
         return $this->userGroups;
     }
 
@@ -89,28 +96,35 @@ class ThemeInfo {
      *
      * @return array
      */
-    public function toArray(): array {
+    public function toArray(): array
+    {
         return [
             'id' => $this->id,
             'default' => $this->default,
             'bundled' => $this->bundled,
             'user-groups' => $this->userGroups,
-			"kind" => $this->kind,
+            "kind" => $this->kind,
         ];
     }
 
     /**
-	 * @return string The theme kind, i.e. "dark" or "light
-	 */
-    public function getKind(): string {
-    	return $this->kind;
-	}
+     * @return string The theme kind, i.e. "dark" or "light
+     */
+    public function getKind(): string
+    {
+        if (array_key_exists($this->kind, $this->recognizedKinds)) {
+            return $this->kind;
+        }
+        return $this->defaultKind;
 
-	/**
-	 * @return string The class to add describing the theme's kind
-	 */
-	public function kindClassName(): string {
-		return $this->recognizedKinds[$this->getKind()];
-	}
+    }
+
+    /**
+     * @return string The class to add describing the theme's kind
+     */
+    public function kindClassName(): string
+    {
+        return $this->recognizedKinds[$this->getKind()];
+    }
 
 }
